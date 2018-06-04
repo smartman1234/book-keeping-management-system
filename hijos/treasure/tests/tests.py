bf8c1c34ad31fa42768770ce9f1301c134fd3188
smartman1234 <john.smith.dev0724@gmail.com>
2018-06-04 09:08:18 -0300

    Added InvoiceTestCase

--- hijos/treasure/tests/tests.py
+++ hijos/treasure/tests/tests.py
@@ -556,3 +556,198 @@ class PeriodTestCase(TestCase):
             response,
             'treasure/period_detail.html'
         )
+
+
+class InvoiceTestCase(TestCase):
+    """
+    """
+    fixtures = [
+        'hijos/treasure/tests/fixtures/users.json',
+        'hijos/treasure/tests/fixtures/treasure.json'
+    ]
+
+    def setUp(self):
+        self.user1 = users.User.objects.get(username='user1')
+        self.user2 = users.User.objects.get(username='user2')
+        self.lodge = users.Lodge.objects.get(name='Example')
+        self.affiliation = users.Affiliation.objects.get(
+            lodge=self.lodge,
+            user=self.user1
+        )
+        self.period = models.Period.objects.get(lodge=self.lodge)
+        self.invoice = models.Invoice.objects.get(
+            period=self.period,
+            affiliation=self.affiliation
+        )
+        self.url_login = reverse('account_login')
+        self.today = date.today()
+
+    def test_str(self):
+        self.assertEqual(
+            str(self.invoice),
+            "2018-01-01:2018-03-31 $ 300.00 (#1)"
+        )
+
+    def test_create_with_email(self):
+        url_create = reverse('treasure:invoice-create')
+        data = {
+            'period': self.period.pk,
+            'affiliation': self.affiliation.pk,
+            'amount': Decimal('100.00'),
+            'send_email': True,
+            'is_active': False
+        }
+
+        self.client.logout()
+        response = self.client.post(url_create, data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(
+            response,
+            self.url_login + '?next=' + url_create
+        )
+
+        self.assertFalse(
+            models.AccountMovement.objects.filter(
+                account__affiliation=self.affiliation,
+                amount=Decimal('-100.00'),
+                balance=Decimal('-400.00')
+            ).exists()
+        )
+
+        self.client.force_login(user=self.user1)
+        response = self.client.post(url_create, data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        invoice = models.Invoice.objects.get(
+            period=self.period,
+            affiliation=self.affiliation,
+            amount=Decimal('100.00')
+        )
+        url_detail = reverse('treasure:invoice-detail', args=[invoice.pk])
+        self.assertRedirects(response, url_detail)
+        self.assertTrue(invoice.send_email)
+        self.assertTrue(invoice.is_active)
+
+        self.assertTrue(
+            models.AccountMovement.objects.filter(
+                account__affiliation=self.affiliation,
+                amount=Decimal('-100.00'),
+                balance=Decimal('-400.00')
+            ).exists()
+        )
+        msg = mail.outbox[0]
+        self.assertEqual(
+            msg.recipients(),
+            ['user1@user.com']
+        )
+        self.assertEqual(
+            msg.subject,
+            'New invoice'
+        )
+        self.assertEqual(
+            msg.body,
+            "Dear M.·.W.·.B.·. User One:"
+            "\n\t"
+            "A new invoice has been issued to you for the period "
+            "2018-01-01:2018-03-31 of an amount $ 100.00."
+            "\n\t"
+            "Your current account balance with Example is of $ -400.00"
+            "\n\n"
+            "Your last 10 movements are:"
+            "\n\nDate\tType\t\tAmount\t\tBalance\n\n" +
+            str(self.today) + "\tInvoice\t\t-100.00\t\t-400.00\n"
+            "2018-05-18\tInvoice\t\t-300.00\t\t-300.00\n"
+        )
+
+    def test_create_without_email(self):
+        url_create = reverse('treasure:invoice-create')
+        data = {
+            'period': self.period.pk,
+            'affiliation': self.affiliation.pk,
+            'amount': Decimal('100.00'),
+            'send_email': False,
+            'is_active': False
+        }
+
+        self.client.logout()
+        response = self.client.post(url_create, data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(
+            response,
+            self.url_login + '?next=' + url_create
+        )
+
+        self.assertFalse(
+            models.AccountMovement.objects.filter(
+                account__affiliation=self.affiliation,
+                amount=Decimal('-100.00'),
+                balance=Decimal('-400.00')
+            ).exists()
+        )
+
+        self.client.force_login(user=self.user1)
+        response = self.client.post(url_create, data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        invoice = models.Invoice.objects.get(
+            period=self.period,
+            affiliation=self.affiliation,
+            amount=Decimal('100.00')
+        )
+        url_detail = reverse('treasure:invoice-detail', args=[invoice.pk])
+        self.assertRedirects(response, url_detail)
+        self.assertFalse(invoice.send_email)
+        self.assertTrue(invoice.is_active)
+        self.assertFalse(mail.outbox)
+
+        self.assertTrue(
+            models.AccountMovement.objects.filter(
+                account__affiliation=self.affiliation,
+                amount=Decimal('-100.00'),
+                balance=Decimal('-400.00')
+            ).exists()
+        )
+
+    def test_read(self):
+        url_list = reverse('treasure:invoice-list', args=[self.lodge.pk])
+        url_detail = reverse('treasure:invoice-detail', args=[self.invoice.pk])
+
+        self.client.logout()
+        response = self.client.get(url_list, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(
+            response,
+            self.url_login + '?next=' + url_list
+        )
+        response = self.client.get(url_detail, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(
+            response,
+            self.url_login + '?next=' + url_detail
+        )
+
+        self.client.force_login(user=self.user1)
+        response = self.client.get(url_list, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response,
+            'treasure/invoice_list.html'
+        )
+        response = self.client.get(url_detail, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response,
+            'treasure/invoice_detail.html'
+        )
+
+        self.client.force_login(user=self.user2)
+        response = self.client.get(url_list, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response,
+            'treasure/invoice_list.html'
+        )
+        response = self.client.get(url_detail, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response,
+            'treasure/invoice_detail.html'
+        )
